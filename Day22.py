466fb7fec90fe57bec521c39b0881e08531fd2d0
Tim Child <timjchild@phas.ubc.ca>
2021-01-06 16:22:59 -0800

    Day22 done

--- Day22.py
+++ Day22.py
@@ -1,4 +1,5 @@
 import numpy as np
+import time
 import copy
 from typing import List, Tuple
 import pandas as pd
@@ -54,6 +55,19 @@ def play_game1(player1: Player, player2: Player) -> Tuple[int, List[int]]:
     return winner, win_hand
 
 
+def hand_length_condition(p1hand, p2hand):
+    if any([len(hand) == 0 for hand in [p1hand, p2hand]]):
+        if len(p1hand) == 0 and len(p2hand) != 0:
+            game_winner = 2
+        elif len(p2hand) == 0 and len(p1hand) != 0:
+            game_winner = 1
+        else:
+            raise RuntimeError(f'ERROR: {p1hand, p2hand}')
+        return True, game_winner
+    else:
+        return False, None
+
+
 class RecursiveGame:
     def __init__(self, game_id):
         self.game_id = game_id
@@ -62,63 +76,53 @@ class RecursiveGame:
 
     def play_game(self, p1hand: List[int], p2hand: List[int]):
         hands = (p1hand, p2hand)
-        if tuple(hands) in self.previous_hands:
-            rule = 'Infinite Recursion Rule'
-            game_winner = 1
-        elif any([len(hand) == 0 for hand in hands]):
-            rule = 'No cards left rule'
-            if len(p1hand) == 0 and len(p2hand) != 0:
-                game_winner = 2
-            elif len(p2hand) == 0 and len(p1hand) != 0:
-                game_winner = 1
-            else:
-                raise RuntimeError(f'ERROR: {p1hand, p2hand}')
-            print(
-                f'{"   " * (self.game_id - 1)}Game {self.game_id} Winner: Player1 Wins through {rule}: {p1hand, p2hand}')
-        else:
-            round_winner = self.play_round(*hands)
-
-            self.previous_hands = []
-
-            game_winner, rule = self.play_game(*hands)
-        return game_winner, rule
+        game_won, round_winner = False, None
+        while game_won is False:
+            round_winner, game_won = self.play_round(*hands)
+        game_winner = round_winner
+        return game_winner
 
     def play_round(self, p1hand: List[int], p2hand: List[int]):
         hands = (p1hand, p2hand)
-        self.previous_hands.append(tuple(hands))
-        cards = [hand.pop(0) for hand in hands]
-        if all([len(h) >= c for c, h in zip(cards, hands)]):
-            new_game = RecursiveGame(self.game_id+1)
-            new_hands = [hand[:c] for hand, c in zip(hands, cards)]
-            print(f'!!! Playing sub game {self.game_id+1} to determine round. p1, p2, c1, c2 = {p1hand, p2hand, *cards}\n'
-                  f'New Hands: {new_hands}')
-            round_winner, rule = new_game.play_game(*new_hands)
-            print(f'==== Ended sub game {self.game_id+1} with round_winner, rule: {round_winner, rule}')
+        length_condition, win_hand = hand_length_condition(p1hand, p2hand)
+        if length_condition:
+            rule = 'Hand Length Rule'
+            game_winner = win_hand
+            # print(
+            #     f'{"   " * (self.game_id - 1)}Game {self.game_id} Winner: Player1 Wins through {rule}: {p1hand, p2hand}')
+            return game_winner, True
+        elif tuple([tuple(hand) for hand in hands]) in self.previous_hands:
+            rule = 'Infinite Recursion Rule'
+            game_winner = 1
+            # print(
+            #     f'{"   " * (self.game_id - 1)}Game {self.game_id} Winner: Player1 Wins through {rule}: {p1hand, p2hand}')
+            return game_winner, True
         else:
-            if cards[0] > cards[1]:
-                round_winner, card_val = 1, cards[0]
-            elif cards[1] > cards[0]:
-                round_winner, card_val = 2, cards[1]
+            self.previous_hands.append(tuple([tuple(hand) for hand in hands]))
+            cards = [hand.pop(0) for hand in hands]
+            if all([len(h) >= c for c, h in zip(cards, hands)]):
+                new_game = RecursiveGame(self.game_id+1)
+                new_hands = [hand[:c] for hand, c in zip(hands, cards)]
+                # print(f'!!! Playing sub game {self.game_id+1} to determine round. p1, p2, c1, c2 = {p1hand, p2hand, *cards}\n'
+                #       f'New Hands: {new_hands}')
+                round_winner = new_game.play_game(*new_hands)
+                rule = 'Sub Game Win'
+                # print(f'==== Ended sub game {self.game_id+1} with round_winner, rule: {round_winner, rule}')
             else:
-                raise RuntimeError(f'ERROR1: p1, p2, cards = {p1hand, p2hand, cards}')
-            print(f'{"   "*(self.game_id-1)}Game {self.game_id} Round Winner : Not Enough Cards Winner is Player {round_winner} with card_val {card_val}')
-        if round_winner == 1:
-            hands[0].extend(cards)
-        elif round_winner == 2:
-            hands[1].extend(reversed(cards))
-        else:
-            raise RuntimeError(f'ERROR2: p1, p2, cards, round_winner = {p1hand, p2hand, cards, round_winner}')
-        return round_winner
-
-    # def play_full_game(self, player1: Player, player2: Player, show_progress=True):
-    #     p1, p2 = player1, player2
-    #     if show_progress:
-    #         print(p1.hand, '\n', p2.hand)
-    #     while
-    #         self.play_round(p1.hand, p2.hand)
-    #         self.previous_hands = []
-    #         if show_progress:
-    #             print(p1.hand, '\n', p2.hand)
+                if cards[0] > cards[1]:
+                    round_winner, card_val = 1, cards[0]
+                elif cards[1] > cards[0]:
+                    round_winner, card_val = 2, cards[1]
+                else:
+                    raise RuntimeError(f'ERROR1: p1, p2, cards = {p1hand, p2hand, cards}')
+                # print(f'{"   "*(self.game_id-1)}Game {self.game_id} Round Winner : Not Enough Cards Winner is Player {round_winner} with card_val {card_val}')
+            if round_winner == 1:
+                hands[0].extend(cards)
+            elif round_winner == 2:
+                hands[1].extend(reversed(cards))
+            else:
+                raise RuntimeError(f'ERROR2: p1, p2, cards, round_winner = {p1hand, p2hand, cards, round_winner}')
+            return round_winner, False
 
 
 def play_game2(player1: Player, player2: Player, show_progress=True) -> Tuple[int, List[int]]:
@@ -148,22 +152,12 @@ def get_score(winning_hand):
 
 
 if __name__ == '__main__':
-    fp = 'Day22_Input_test.txt'
-    # fp = 'Day22_Input.txt'
+    # fp = 'Day22_Input_test.txt'
+    fp = 'Day22_Input.txt'
 
     h1, h2 = get_hands(fp)
     p1 = Player(h1)
     p2 = Player(h2)
-
-    # print(p1.hand, p2.hand)
-    # take_turn(p1.hand, p2.hand)
-    # print(p1.hand, '\n', p2.hand)
-    # take_turn(p1.hand, p2.hand)
-    # print(p1.hand, '\n', p2.hand)
-    # take_turn(p1.hand, p2.hand)
-    # print(p1.hand, '\n', p2.hand)
-    # take_turn(p1.hand, p2.hand)
-    # print(p1.hand, '\n', p2.hand)
 
     winner, win_hand = play_game1(p1, p2)
     print(f'\n\nPart 1:\nWinning score: {get_score(win_hand)}\n\n')
@@ -172,15 +166,19 @@ if __name__ == '__main__':
     p1, p2 = [Player(h) for h in (h1, h2)]
     p2game = RecursiveGame(1)
 
-    # p2game.play_game(p1.hand, p2.hand)
+    t1 = time.perf_counter()
+    winner = p2game.play_game(p1.hand, p2.hand)
+    if winner == 1:
+        win_hand = p1.hand
+    elif winner == 2:
+        win_hand = p2.hand
+    else:
+        win_hand = None
 
-    print(p1.hand, '\n', p2.hand)
-    for i in range(79):
-        p2game.play_round(p1.hand, p2.hand)
-        p2game.previous_hands = []
-        print(p1.hand, '\n', p2.hand)
-    # winner, win_hand = play_game2(p1, p2, show_progress=True)
-    # print(f'\n\nPart 2:\nWinning score: {get_score(win_hand)}\n\n')
+
+
+    print(f'\n\nPart 2:\nWinning score: {get_score(win_hand)}\n\n')
+    print(f'Took {t1-time.perf_counter():.1f}s')
 
 
 
